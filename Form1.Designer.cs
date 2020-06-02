5f743f06cd0a89678b005e531c41be376748f4e1
SkyShadowMeadow <skyshadowmeadow.91@gmail.com>
2020-06-02 15:43:54 +0300

    добавлена экзаменационная работа

---
+++ Form1.Designer.cs
@@ -0,0 +1,374 @@
+﻿namespace Algorithms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox6 = new System.Windows.Forms.RichTextBox();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(327, 276);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.numericUpDown1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.richTextBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(319, 250);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Решето Эратосфена";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 209);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(307, 33);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Показать все простые числа";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(111, 145);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            2000000000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(202, 20);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите число";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(6, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(307, 129);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.numericUpDown2);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.richTextBox5);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(319, 250);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Числа фибоначчи";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(105, 116);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            90,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(211, 20);
+            this.numericUpDown2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Введите число";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 204);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(307, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Рассчитать последовательность";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox5
+            // 
+            this.richTextBox5.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox5.Name = "richTextBox5";
+            this.richTextBox5.Size = new System.Drawing.Size(313, 96);
+            this.richTextBox5.TabIndex = 0;
+            this.richTextBox5.Text = resources.GetString("richTextBox5.Text");
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.button3);
+            this.tabPage3.Controls.Add(this.label4);
+            this.tabPage3.Controls.Add(this.numericUpDown4);
+            this.tabPage3.Controls.Add(this.label3);
+            this.tabPage3.Controls.Add(this.richTextBox6);
+            this.tabPage3.Controls.Add(this.numericUpDown3);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(319, 250);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Алгоритм Евклида";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 210);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(313, 37);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Найти наибольший общий делитель";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(0, 171);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Введите второе  число";
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(142, 167);
+            this.numericUpDown4.Maximum = new decimal(new int[] {
+            2000000000,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(174, 20);
+            this.numericUpDown4.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(0, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Введите первое число";
+            // 
+            // richTextBox6
+            // 
+            this.richTextBox6.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox6.Name = "richTextBox6";
+            this.richTextBox6.Size = new System.Drawing.Size(313, 124);
+            this.richTextBox6.TabIndex = 1;
+            this.richTextBox6.Text = resources.GetString("richTextBox6.Text");
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(142, 138);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            2000000000,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(174, 20);
+            this.numericUpDown3.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.richTextBox2);
+            this.groupBox1.Location = new System.Drawing.Point(323, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(248, 91);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Решето эратосфена";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(6, 19);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(238, 72);
+            this.richTextBox2.TabIndex = 0;
+            this.richTextBox2.Text = "";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.richTextBox3);
+            this.groupBox2.Location = new System.Drawing.Point(326, 100);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(248, 83);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Числа Фибоначчи";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(6, 19);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(238, 62);
+            this.richTextBox3.TabIndex = 0;
+            this.richTextBox3.Text = "";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.richTextBox4);
+            this.groupBox3.Location = new System.Drawing.Point(327, 189);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(248, 81);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Алгоритм Эвклида";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(6, 20);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(238, 62);
+            this.richTextBox4.TabIndex = 0;
+            this.richTextBox4.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 276);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Алгоритмы";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox6;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Algorithms
+{
+    
+
+    public partial class Form1 : Form
+    {
+        private delegate int[] AsyncEratosphene(int max);
+        private delegate long[] AsyncFibonacci(int n);
+        private delegate int AsyncEvklid(int number1, int number2);
+        private string eratospheneResult;
+        private string fibonacciResult;
+        private string evklidResult;
+
+        //Методы для рассчета алгоритмов
+        //В каждом методе предусмотрена искусственная задержка на 8 сек для демонстрации работы многопоточности
+        #region Eratosphene
+        private int[] Eratosphene (int max)
+        {
+            bool[] simpleNumbers = new bool[max + 1];
+            for (int i = 2; i <= max; i++) simpleNumbers[i] = true;
+
+            for (int i = 2; i <= max; i++)
+            {
+                if (simpleNumbers[i])
+                {
+                    for (int j = i * 2; j <= max; j += i)
+                        simpleNumbers[j] = false;
+                }
+            }
+            int numberOfTrue = 0;
+            for (int i = 2; i <= max; i++)
+            {
+                if (simpleNumbers[i] == true)
+                {
+                    numberOfTrue++;
+                }
+
+            }
+            int[] result = new int[numberOfTrue];
+            int k = 0;
+            for (int i = 2; i <= max; i++)
+            {
+                if (simpleNumbers[i] == true)
+                {
+                    result[k] = i;
+                    k++;
+                }
+            }
+            System.Threading.Thread.Sleep(8000);
+            return result;
+        }
+        #endregion
+        #region Fibonacci
+        private long[] Fibonacci(int n)
+        {
+            long[] result = new long[n];
+            result[0] = 0;
+            result[1] = 1;
+            for (int j = 2; j < n; j++)
+            {
+                result[j] = result[j - 1] + result[j - 2];
+            }
+            System.Threading.Thread.Sleep(8000);
+            return result;
+        }
+        #endregion
+        #region Evklid
+        private int simpleEvklid(int number1, int number2)
+        {
+            while ((number1 != 0) && (number2 != 0))
+            {
+                if (number1 > number2)
+                    number1 -= number2;
+                else
+                    number2 -= number1;
+            }
+            System.Threading.Thread.Sleep(9000);
+            return Math.Max(number1, number2);
+        }
+        #endregion
+
+        //Методы для возврата в основной поток
+        private void CallBackEratosphene(IAsyncResult ar)
+        {
+            AsyncEratosphene eratospheneDelegate = (AsyncEratosphene)ar.AsyncState;
+            string str = String.Format("Сумма чисел равна: {0}", string.Join(",", eratospheneDelegate.EndInvoke(ar)));
+            this.eratospheneResult = str;
+            MethodInvoker mi = new MethodInvoker(this.UpdateUI);
+            this.BeginInvoke(mi);
+        }
+        private void CallBackFibonacci(IAsyncResult ar)
+        {
+            AsyncFibonacci FibonacciDelegate = (AsyncFibonacci)ar.AsyncState;
+            string str = String.Format("Последовательность фибоначчи равна: {0}", string.Join(",", FibonacciDelegate.EndInvoke(ar)));
+            this.fibonacciResult = str;
+            MethodInvoker mi2 = new MethodInvoker(this.UpdateUI2);
+            this.BeginInvoke(mi2);
+        }
+        private void CallBackEvklid(IAsyncResult ar)
+        {
+            AsyncEvklid evklidiDelegate = (AsyncEvklid)ar.AsyncState;
+            string str = String.Format("Наибольший общий делитель равен: {0}", evklidiDelegate.EndInvoke(ar));
+            this.evklidResult = str;
+            MethodInvoker mi3 = new MethodInvoker(this.UpdateUI3);
+            this.BeginInvoke(mi3);
+        }
+
+        //Методы для передачи результатов в соответствующие RichTextBox
+        private void UpdateUI()
+        {
+            this.richTextBox2.Text = this.eratospheneResult;
+        }
+        private void UpdateUI2()
+        {
+            this.richTextBox3.Text = this.fibonacciResult;        
+        }
+        private void UpdateUI3()
+        {
+            this.richTextBox4.Text = this.evklidResult;
+        }
+
+        //Запуск расчета алгоритмов
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int max;
+            try
+            {
+                max = (int)numericUpDown1.Value;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Введен неверный тип данных. Введите число!");
+                numericUpDown1.Value = 0;
+                return;
+            }
+
+            AsyncEratosphene eratospheneDelegate = new AsyncEratosphene(Eratosphene);
+            AsyncCallback cb1 = new AsyncCallback(CallBackEratosphene);
+            IAsyncResult asyncResult = eratospheneDelegate.BeginInvoke(max, cb1, eratospheneDelegate);
+
+            
+
+                    
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int n;
+            try
+            {
+                n = (int)numericUpDown2.Value;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Введен неверный тип данных. Введите число!");
+                numericUpDown2.Value = 0;
+                return;
+            }
+
+            AsyncFibonacci FibonacciDelegate = new AsyncFibonacci(Fibonacci);
+            AsyncCallback cb2 = new AsyncCallback(CallBackFibonacci);
+            IAsyncResult asyncResult = FibonacciDelegate.BeginInvoke(n, cb2, FibonacciDelegate);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int number1, number2;
+            try
+            {
+                number1 = (int)numericUpDown3.Value;
+                number2 = (int)numericUpDown4.Value;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Введен неверный тип данных. Введите число!");
+                numericUpDown3.Value = 0;
+                numericUpDown4.Value = 0;
+                return;
+            }
+
+            AsyncEvklid evklidiDelegate = new AsyncEvklid(simpleEvklid);
+            AsyncCallback cb3 = new AsyncCallback(CallBackEvklid);
+            IAsyncResult asyncResult = evklidiDelegate.BeginInvoke(number1, number2, cb3, evklidiDelegate);
+        }
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
